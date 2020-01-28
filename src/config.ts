e5824a251824c3aa2898adde0153b4a0bb3ea811
naidishuli <naidishuli@gmail.com>
2020-01-28 11:33:06 +0100

    kubernetes

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,21 @@
 import { config } from 'dotenv-safe';
 import path from 'path';
+import fs from "fs";
 
 const env = process.env.NODE_ENV || 'development';
-const envFile = '.env';
+let envFile = '.env.example';
 
-config({
-  path: path.join(__dirname, `../${envFile}`),
-  sample: path.join(__dirname, '../.env.example'),
-});
+const envPath = path.join(__dirname, `../.env`);
+if (fs.existsSync(envPath)){
+  envFile = '.env';
+}
+
+if (process.env.NODE_ENV != 'production') {
+  config({
+    path: path.join(__dirname, `../${envFile}`),
+    sample: path.join(__dirname, '../.env.example'),
+  });
+}
 
 const port = process.env.API_PORT as string;
 const apiBasePath = process.env.API_BASE_PATH as string;
