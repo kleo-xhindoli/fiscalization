ddf980301741d4454004a690410d7b6b61ec2640
kxhindoli <kxhindoli@ritech.co>
2020-02-22 14:25:06 +0100

    setup tests for SOAP

--- src/tests/integration/setup-tests.ts
+++ src/tests/integration/setup-tests.ts
@@ -1,6 +1,7 @@
 import config from '../../config';
 import logger from '../../api/config/logger';
 import app from '../../api/config/express';
+import initSoap from '../../api/config/soap';
 
 const { port, env } = config;
 
@@ -8,6 +9,16 @@ export function initializeServer() {
   return app.listen(port, () => {
     logger.info(`server started on port ${port} (${env})`);
   });
+}
+
+export async function initializeSOAP() {
+  const client = await initSoap();
+  logger.info('SOAP Initialized');
+  return client;
+}
+
+export async function initializeMockSOAP() {
+  // TODO
 }
 
 export default app;
